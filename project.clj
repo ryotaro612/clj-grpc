c3ab73639b7da8ff676cfda81a2480fe311bf627
Ryotaro <ryotaro612.ooo@gmail.com>
2024-05-06 11:10:50 +0900

    chore: upgrade depenencies to the lastest ones.

--- project.clj
+++ project.clj
@@ -5,23 +5,24 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0"
             :year 2022
             :key "apache-2.0"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [io.pedestal/pedestal.service "0.5.9"]
+  :dependencies [[org.clojure/clojure "1.11.3"]
+                 [io.pedestal/pedestal.service "0.6.3"]
 
                  ;; -- PROTOC-GEN-CLOJURE --
-                 [io.github.protojure/grpc-server "2.0.1"]
-                 [io.github.protojure/google.protobuf "2.0.0"]
+                 [io.github.protojure/grpc-server "2.8.2"]
+                 [io.github.protojure/google.protobuf "2.0.1"]
 
-                 [ch.qos.logback/logback-classic "1.2.9"]
-                 [org.slf4j/jul-to-slf4j "1.7.32"]
-                 [org.slf4j/jcl-over-slf4j "1.7.32"]
-                 [org.slf4j/log4j-over-slf4j "1.7.32"]]
+                 [ch.qos.logback/logback-classic "1.5.6"]
+                 [org.slf4j/jul-to-slf4j "2.0.13"]
+                 [org.slf4j/jcl-over-slf4j "2.0.13"]
+                 [org.slf4j/log4j-over-slf4j "2.0.13"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   :source-paths ["src", "gen"]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "pedestal-grpc.server/run-dev"]}
                    :resource-paths ["dev/resources"]
                    :source-paths ["dev/src"]
-                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.9"]]}
+                   :dependencies [[io.pedestal/pedestal.service-tools "0.6.3"]]}
              :uberjar {:aot [pedestal-grpc.server]}}
+  :plugins [["lein-ancient" "0.7.0"]]
   :main ^{:skip-aot true} pedestal-grpc.server)
